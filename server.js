5414da95948e7a2f4537f495bf1ab17b6ad5744e
Matt Linder <mjlinder218@gmail.com>
2021-08-06 09:35:17 -0700

    Fixed mongoose.connect statement

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
+// From https://stackoverflow.com/questions/14183611/mongoose-always-returning-an-empty-array-nodejs
+db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback(){
+    console.log("CONNECTED");
+});
+
+
+// routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);

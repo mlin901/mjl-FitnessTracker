805a87850123e2d1ed873ab3ccd5319add5c4a48
Matt Linder <mjlinder218@gmail.com>
2021-08-08 19:30:27 -0700

    Added more require statements, mongoose setup, etc.

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-var express = require("express");
-var mongoose = require("mongoose");
+const path = require('path');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var app = express();
 var PORT = process.env.PORT || 3000;
 
+const db = require("./models");
 
-app.use(express.static("public"));
+const app = express();
+
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true
-});
-// From https://stackoverflow.com/questions/14183611/mongoose-always-returning-an-empty-array-nodejs
-db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function callback(){
-    console.log("CONNECTED");
-});
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // routes
 app.use(require("./routes/apiRoutes.js"));

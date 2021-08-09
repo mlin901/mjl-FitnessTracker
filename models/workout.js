daacd1064fabcce9b10043362c31681d10d55bbe
Matt Linder <mjlinder218@gmail.com>
2021-08-08 19:22:26 -0700

    Changed from two models (Workout and Exercise) to one (Workout with exercises in array)

--- models/workout.js
+++ models/workout.js
@@ -5,15 +5,41 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.Now
+    default: Date.now
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise type is required"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required"
+      },
+      distance: Number,
+      weight: Number,
+      sets: Number,
+      reps: Number,
+      duration: Number,
     }
   ]
 });
+
+// const WorkoutSchema = new Schema({
+//   day: {
+//     type: Date,
+//     default: Date.Now
+//   },
+//   exercises: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: "Exercise"
+//     }
+//   ]
+// });
 
 const Workout = mongoose.model("Workout", WorkoutSchema, "workouts");
 

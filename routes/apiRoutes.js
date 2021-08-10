1a7acc831659979b9657a90c4dfe73a3d33fc16d
Matt Linder <mjlinder218@gmail.com>
2021-08-10 06:54:20 -0700

    Fixed Total Duration field problem

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,13 @@ const Workout = require("../models/workout.js");
 router.get("/api/workouts/range", async (req, res) => {
   Workout.aggregate([
     {$addFields: { totalDuration: { $sum: '$exercises.duration' } } } 
-  ]).sort({day: -1})
+  ])
+  .sort({day: -1})
   .limit(7)
   .sort({day: 1})
   .then(aggregateInfo => {
-    console.log('============================');
-    console.log(aggregateInfo);
+    // console.log('============================');
+    // console.log(aggregateInfo);
     res.json(aggregateInfo);
   })
   .catch(err => {
@@ -20,10 +21,15 @@ router.get("/api/workouts/range", async (req, res) => {
 });
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ day: -1 })
-    .then(foundWorkout => {
-      res.json(foundWorkout);
+  Workout.aggregate([
+      {$addFields: { totalDuration: { $sum: '$exercises.duration' } } } 
+  ])
+  // Workout.find({})
+  //   .sort({ day: -1 }) 
+    .then(foundWorkouts => {
+      // console.log('?????????????????????????');
+      // console.log(foundWorkouts);
+      res.json(foundWorkouts);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -31,6 +37,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.post("/api/workouts", ({ body }, res) => {
+  // console.log(' *****PUT API-WORKOUTS*******');
   Workout.create(body)
     .then(newWorkout => {
       res.json(newWorkout);
@@ -41,12 +48,29 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.put("/api/workouts/:id", ({ body, params}, res) => {
+  // console.log(' *****PUT API-WORKOUTS-:id*******');
   Workout.findOneAndUpdate(
     { _id: params.id },
     { $push: { exercises: body } },
+    // { $addFields: { totalDuration: { $sum: '$exercises.duration' } } },
     { new: true } 
   ).then(workoutUpdate => {
+
+
+    // workoutUpdate.aggregate([
+    //   {$addFields: { totalDuration: { $sum: '$exercises.duration' } } }, 
+    //   { //options
+    //     returnNewDocument: true,
+    //     new: true,
+    //     strict: false
+    //   }
+    // ]);
+
+    // console.log(workoutUpdate);
+    // console.log(workoutUpdate.totalDuration);
+
     res.json(workoutUpdate);
+
   })
   .catch(err => {
     res.status(404).json(err);
